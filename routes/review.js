9b39526a02faec1d9b8ebac0ccba329fec0ae3df
Leejunhang <share97@naver.com>
2023-10-19 18:31:50 +0900

    ver16

--- routes/review.js
+++ routes/review.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var db = require('../db');
 const { post } = require('.');
 
-router.get('/list.json', function(req, res){  //테스트 localhot:3000/review/list.json?bid=118
+router.get('/list.json', function(req, res){  //테스트 localhost:3000/review/list.json?bid=124&page=1
     const bid=req.query.bid;
     const page=parseInt(req.query.page);
     const start=(page-1)*5;
-    const sql='select * from review where bid=? order by rid desc limit ?, 5';
+    const sql='select * from view_review where bid=? limit ?,5';
     db.get().query(sql, [bid, start], function(err, rows){
         res.send(rows);
     });
@@ -32,5 +32,23 @@ router.post("/insert", function(req, res){
     });
 });
 
+//리뷰 삭제
+router.post("/delete", function(req, res){
+    const rid=req.body.rid;
+    const sql='delete from review where rid=?'
+    db.get().query(sql, [rid], function(err){
+        res.sendStatus(200);
+    });
+});
+
+//리뷰 수정
+router.post('/update', function(req, res){
+    const rid=req.body.rid;
+    const contents=req.body.contents;
+    const sql='update review set contents=? where rid=?'
+    db.get().query(sql, [contents, rid], function(err){
+        res.sendStatus(200);
+    });
+});
 
 module.exports = router;
